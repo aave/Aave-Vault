0585de4d5dd199b3e0d90176910778d70fc37e39
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2022-10-24 20:36:44 +0200

    feat: Add fee setting for owner

--- src/ATokenVault.sol
+++ src/ATokenVault.sol
@@ -5,9 +5,31 @@ import "forge-std/Test.sol";
 
 import {ERC4626} from "solmate/mixins/ERC4626.sol";
 import {ERC20} from "solmate/tokens/ERC20.sol";
+import {Ownable} from "openzeppelin/access/Ownable.sol";
 
-contract ATokenVault is ERC4626 {
+contract ATokenVault is ERC4626, Ownable {
+    uint256 internal constant SCALE = 1e18;
+
+    uint256 public lastUpdated;
+    uint256 public lastVaultBalance;
+    uint256 public fee;
+
+    event FeeUpdated(uint256 oldFee, uint256 newFee);
+
+    // TODO add dynamic strings for name/symbol
     constructor(ERC20 aToken) ERC4626(aToken, "Wrapped [aTKN]", "w[aTKN]") {}
+
+    // TODO take fee on withdraw/redeem
+
+    // TODO refactor errors
+    function setFee(uint256 _newFee) public onlyOwner {
+        require(_newFee < SCALE, "FEE_TOO_HIGH");
+
+        uint256 oldFee = fee;
+        fee = _newFee;
+
+        emit FeeUpdated(oldFee, _newFee);
+    }
 
     function totalAssets() public view override returns (uint256) {
         return 0;
