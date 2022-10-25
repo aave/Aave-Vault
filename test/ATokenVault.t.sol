dab2b24a995adcab15a25fa67f49a8cf17671959
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2022-10-25 14:53:42 +0200

    feat: Fork test and vault deploy working

--- test/ATokenVault.t.sol
+++ test/ATokenVault.t.sol
@@ -9,17 +9,21 @@ import {IPoolAddressesProvider} from "aave/interfaces/IPoolAddressesProvider.sol
 
 contract ATokenVaultTest is Test {
     // Forked tests using Polygon
-    uint256 polygonFork;
+    address public constant POLYGON_DAI = 0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063;
+    address public constant POLYGON_ADAI = 0x82E64f49Ed5EC1bC6e43DAD4FC8Af9bb3A2312EE;
+    address public constant POLYGON_AAVE_POOL = 0x794a61358D6845594F94dc1DB02A252b5b4814aD;
     address public constant POLYGON_POOL_ADDRESSES_PROVIDER = 0xa97684ead0e402dC232d5A977953DF7ECBaB3CDb;
+    uint256 polygonFork;
     ATokenVault vault;
     ERC20 dai;
 
     function setUp() public {
         polygonFork = vm.createFork(vm.envString("POLYGON_RPC_URL"));
+        vm.selectFork(polygonFork);
+        dai = ERC20(POLYGON_DAI);
     }
 
     function testForkWorks() public {
-        vm.selectFork(polygonFork);
         assertEq(vm.activeFork(), polygonFork);
     }
 
@@ -36,11 +40,10 @@ contract ATokenVaultTest is Test {
     //////////////////////////////////////////////////////////////*/
 
     function testDeploySucceedsWithValidParams() public {
-        // TODO fork dai
-        // dai = ERC20();
-
         vault = new ATokenVault(dai, IPoolAddressesProvider(POLYGON_POOL_ADDRESSES_PROVIDER));
-        // TODO check aToken address and aavePool address set as expected
+        assertEq(address(vault.asset()), POLYGON_DAI);
+        assertEq(address(vault.aToken()), POLYGON_ADAI);
+        assertEq(address(vault.aavePool()), POLYGON_AAVE_POOL);
     }
 
     /*//////////////////////////////////////////////////////////////
