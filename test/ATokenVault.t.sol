ef5b1ccae4ddca465d74fe1347e80ebb6e26d8b5
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2022-11-01 13:47:30 +0200

    feat: Added base supply tests

--- test/ATokenVault.t.sol
+++ test/ATokenVault.t.sol
@@ -4,15 +4,22 @@ pragma solidity 0.8.10;
 import "forge-std/Test.sol";
 
 import {ATokenVault} from "../src/ATokenVault.sol";
+import {IAToken} from "aave/interfaces/IAToken.sol";
 import {ERC20} from "solmate/tokens/ERC20.sol";
 import {IPoolAddressesProvider} from "aave/interfaces/IPoolAddressesProvider.sol";
 
 contract ATokenVaultTest is Test {
     // Forked tests using Polygon for Aave v3
-    address public constant POLYGON_DAI = 0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063;
-    address public constant POLYGON_ADAI = 0x82E64f49Ed5EC1bC6e43DAD4FC8Af9bb3A2312EE;
-    address public constant POLYGON_AAVE_POOL = 0x794a61358D6845594F94dc1DB02A252b5b4814aD;
-    address public constant POLYGON_POOL_ADDRESSES_PROVIDER = 0xa97684ead0e402dC232d5A977953DF7ECBaB3CDb;
+    address constant POLYGON_DAI = 0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063;
+    address constant POLYGON_ADAI = 0x82E64f49Ed5EC1bC6e43DAD4FC8Af9bb3A2312EE;
+    address constant POLYGON_AAVE_POOL = 0x794a61358D6845594F94dc1DB02A252b5b4814aD;
+    address constant POLYGON_POOL_ADDRESSES_PROVIDER = 0xa97684ead0e402dC232d5A977953DF7ECBaB3CDb;
+
+    uint256 constant ONE = 1e18;
+    uint256 constant TEN = 10e18;
+
+    address constant ALICE = address(123);
+    address constant BOB = address(456);
 
     string constant SHARE_NAME = "Wrapped aDAI";
     string constant SHARE_SYMBOL = "waDAI";
@@ -21,11 +28,13 @@ contract ATokenVaultTest is Test {
     uint256 polygonFork;
     ATokenVault vault;
     ERC20 dai;
+    IAToken aDai;
 
     function setUp() public {
         polygonFork = vm.createFork(vm.envString("POLYGON_RPC_URL"));
         vm.selectFork(polygonFork);
         dai = ERC20(POLYGON_DAI);
+        aDai = IAToken(POLYGON_ADAI);
     }
 
     function testForkWorks() public {
@@ -45,21 +54,54 @@ contract ATokenVaultTest is Test {
     //////////////////////////////////////////////////////////////*/
 
     function testDeploySucceedsWithValidParams() public {
-        vault = new ATokenVault(
-            dai,
-            SHARE_NAME,
-            SHARE_SYMBOL,
-            DEFAULT_FEE,
-            IPoolAddressesProvider(POLYGON_POOL_ADDRESSES_PROVIDER)
-        );
-        assertEq(address(vault.asset()), POLYGON_DAI);
-        assertEq(address(vault.aToken()), POLYGON_ADAI);
-        assertEq(address(vault.aavePool()), POLYGON_AAVE_POOL);
+        _deployAndCheckProps();
     }
 
-    function testDepositSuppliesAave() public {}
+    function testDepositSuppliesAave() public {
+        _deployAndCheckProps();
 
-    function testMintSuppliesAave() public {}
+        deal(address(dai), ALICE, ONE);
+
+        assertEq(dai.balanceOf(ALICE), ONE);
+        assertEq(dai.balanceOf(address(vault)), 0);
+        assertEq(aDai.balanceOf(ALICE), 0);
+        assertEq(aDai.balanceOf(address(vault)), 0);
+        assertEq(vault.balanceOf(ALICE), 0);
+
+        vm.startPrank(ALICE);
+        dai.approve(address(vault), ONE);
+        vault.deposit(ONE, ALICE);
+        vm.stopPrank();
+
+        assertEq(dai.balanceOf(ALICE), 0);
+        assertEq(dai.balanceOf(address(vault)), 0);
+        assertEq(aDai.balanceOf(ALICE), 0);
+        assertEq(aDai.balanceOf(address(vault)), ONE);
+        assertEq(vault.balanceOf(ALICE), ONE);
+    }
+
+    function testMintSuppliesAave() public {
+        _deployAndCheckProps();
+
+        deal(address(dai), ALICE, ONE);
+
+        assertEq(dai.balanceOf(ALICE), ONE);
+        assertEq(dai.balanceOf(address(vault)), 0);
+        assertEq(aDai.balanceOf(ALICE), 0);
+        assertEq(aDai.balanceOf(address(vault)), 0);
+        assertEq(vault.balanceOf(ALICE), 0);
+
+        vm.startPrank(ALICE);
+        dai.approve(address(vault), ONE);
+        vault.mint(ONE, ALICE);
+        vm.stopPrank();
+
+        assertEq(dai.balanceOf(ALICE), 0);
+        assertEq(dai.balanceOf(address(vault)), 0);
+        assertEq(aDai.balanceOf(ALICE), 0);
+        assertEq(aDai.balanceOf(address(vault)), ONE);
+        assertEq(vault.balanceOf(ALICE), ONE);
+    }
 
     function testWithdrawNoFee() public {}
 
@@ -86,4 +128,17 @@ contract ATokenVaultTest is Test {
     /*//////////////////////////////////////////////////////////////
                                 TEST UTILS
     //////////////////////////////////////////////////////////////*/
+
+    function _deployAndCheckProps() public {
+        vault = new ATokenVault(
+            dai,
+            SHARE_NAME,
+            SHARE_SYMBOL,
+            DEFAULT_FEE,
+            IPoolAddressesProvider(POLYGON_POOL_ADDRESSES_PROVIDER)
+        );
+        assertEq(address(vault.asset()), POLYGON_DAI);
+        assertEq(address(vault.aToken()), POLYGON_ADAI);
+        assertEq(address(vault.aavePool()), POLYGON_AAVE_POOL);
+    }
 }
