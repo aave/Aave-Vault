290a8c75d09b893158da643743b1c5c1155a43f6
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2022-11-23 16:13:25 +0200

    feat: Added rest of natspec, reordered functions

--- src/ATokenVault.sol
+++ src/ATokenVault.sol
@@ -125,29 +125,6 @@ contract ATokenVault is ERC4626, Ownable {
         shares = _deposit(assets, receiver, depositor);
     }
 
-    function _deposit(
-        uint256 assets,
-        address receiver,
-        address depositor
-    ) internal returns (uint256 shares) {
-        _accrueYield();
-
-        require((shares = previewDeposit(assets)) != 0, "ZERO_SHARES");
-
-        // Need to transfer before minting or ERC777s could reenter.
-        asset.safeTransferFrom(depositor, address(this), assets);
-
-        // Approve and Deposit the received underlying into Aave v3
-        asset.approve(address(aavePool), assets);
-        aavePool.supply(address(asset), assets, address(this), 0);
-
-        lastVaultBalance = aToken.balanceOf(address(this));
-
-        _mint(receiver, shares);
-
-        emit Deposit(msg.sender, receiver, assets, shares);
-    }
-
     /**
      * @notice Mints a specified amount of shares to the receiver, depositing the corresponding amount of assets.
      *
@@ -192,6 +169,212 @@ contract ATokenVault is ERC4626, Ownable {
         assets = _mint(shares, receiver, depositor);
     }
 
+    /**
+     * @notice Withdraws a specified amount of assets from the vault, burning the corresponding amount of shares.
+     *
+     * @param assets The amount of assets to withdraw
+     * @param receiver The address to receive the assets
+     * @param owner The address from which to pull the shares for the withdrawal
+     *
+     * @return shares The amount of shares burnt in the withdrawal process
+     */
+    function withdraw(
+        uint256 assets,
+        address receiver,
+        address owner
+    ) public override returns (uint256 shares) {
+        shares = _withdraw(assets, receiver, owner, false);
+    }
+
+    /**
+     * @notice Withdraws a specified amount of assets from the vault, burning the corresponding amount of shares,
+     * using an EIP721 signature to enable a third-party to call this function on behalf of the owner.
+     *
+     * @param assets The amount of assets to withdraw
+     * @param receiver The address to receive the assets
+     * @param owner The address from which to pull the shares for the withdrawal
+     * @param sig An EIP721 signature from the owner to allow this function to be called on their behalf
+     *
+     * @return shares The amount of shares burnt in the withdrawal process
+     */
+    function withdrawWithSig(
+        uint256 assets,
+        address receiver,
+        address owner,
+        DataTypes.EIP712Signature calldata sig
+    ) public returns (uint256 shares) {
+        unchecked {
+            MetaTxHelpers._validateRecoveredAddress(
+                MetaTxHelpers._calculateDigest(
+                    keccak256(
+                        abi.encode(WITHDRAW_WITH_SIG_TYPEHASH, assets, receiver, owner, sigNonces[owner]++, sig.deadline)
+                    ),
+                    DOMAIN_SEPARATOR()
+                ),
+                owner,
+                sig
+            );
+        }
+        shares = _withdraw(assets, receiver, owner, true);
+    }
+
+    /**
+     * @notice Burns a specified amount of shares from the vault, withdrawing the corresponding amount of assets.
+     *
+     * @param shares The amount of shares to burn
+     * @param receiver The address to receive the assets
+     * @param owner The address from which to pull the shares for the withdrawal
+     *
+     * @return assets The amount of assets withdrawn by the receiver
+     */
+    function redeem(
+        uint256 shares,
+        address receiver,
+        address owner
+    ) public override returns (uint256 assets) {
+        assets = _redeem(shares, receiver, owner, false);
+    }
+
+    /**
+     * @notice Burns a specified amount of shares from the vault, withdrawing the corresponding amount of assets,
+     * using an EIP721 signature to enable a third-party to call this function on behalf of the owner.
+     *
+     * @param shares The amount of shares to burn
+     * @param receiver The address to receive the assets
+     * @param owner The address from which to pull the shares for the withdrawal
+     * @param sig An EIP721 signature from the owner to allow this function to be called on their behalf
+     *
+     * @return assets The amount of assets withdrawn by the receiver
+     */
+    function redeemWithSig(
+        uint256 shares,
+        address receiver,
+        address owner,
+        DataTypes.EIP712Signature calldata sig
+    ) public returns (uint256 assets) {
+        unchecked {
+            MetaTxHelpers._validateRecoveredAddress(
+                MetaTxHelpers._calculateDigest(
+                    keccak256(abi.encode(REDEEM_WITH_SIG_TYPEHASH, shares, receiver, owner, sigNonces[owner]++, sig.deadline)),
+                    DOMAIN_SEPARATOR()
+                ),
+                owner,
+                sig
+            );
+        }
+        assets = _redeem(shares, receiver, owner, true);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                          ONLY OWNER FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * @notice Sets the fee the vault levies on yield earned, only callable by the owner.
+     *
+     * @param newFee The new fee, as a fraction of 1e18.
+     */
+    function setFee(uint256 newFee) public onlyOwner {
+        if (newFee > SCALE) revert Errors.FeeTooHigh();
+
+        uint256 oldFee = fee;
+        fee = newFee;
+
+        emit Events.FeeUpdated(oldFee, newFee);
+    }
+
+    /**
+     * @notice Updates the Aave Pool in this vault to the latest address given by the
+     * Aave Pool Addresses Provider, only callable by the owner
+     *
+     */
+    function updateAavePool() public onlyOwner {
+        address newPool = POOL_ADDRESSES_PROVIDER.getPool();
+        aavePool = IPool(newPool);
+
+        emit Events.AavePoolUpdated(newPool);
+    }
+
+    /**
+     * @notice Withdraws fees earned by the vault, in the form of aTokens, to a specified address. Only callable by the owner.
+     *
+     * @param to The address to receive the fees
+     * @param amount The amount of fees to withdraw
+     *
+     */
+    function withdrawFees(address to, uint256 amount) public onlyOwner {
+        uint256 currentFees = getCurrentFees();
+        if (amount > currentFees) revert Errors.InsufficientFees(); // will underflow below anyway, error msg for clarity
+
+        accumulatedFees = currentFees - amount;
+        lastVaultBalance = aToken.balanceOf(address(this)) - amount;
+        lastUpdated = block.timestamp;
+
+        aToken.transfer(to, amount);
+
+        emit Events.FeesWithdrawn(to, amount);
+    }
+
+    /**
+     * @notice Claims any additional Aave rewards earned from vault deposits. Only callable by the owner.
+     *
+     * @param to The address to receive any rewards tokens.
+     *
+     */
+    function claimAllAaveRewards(address to) public onlyOwner {
+        if (to == address(0)) revert Errors.CannotSendRewardsToZeroAddress();
+
+        address[] memory assets = new address[](1);
+        assets[0] = address(aToken);
+
+        (address[] memory rewardsList, uint256[] memory claimedAmounts) = REWARDS_CONTROLLER.claimAllRewards(assets, to);
+
+        emit Events.AaveRewardsClaimed(to, rewardsList, claimedAmounts);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                          INTERNAL FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    function _accrueYield() internal {
+        // Fees are accrued and claimable in aToken form
+        if (block.timestamp != lastUpdated) {
+            uint256 newVaultBalance = aToken.balanceOf(address(this));
+            uint256 newYield = newVaultBalance - lastVaultBalance;
+            uint256 newFeesEarned = newYield.mulDivDown(fee, SCALE);
+
+            accumulatedFees += newFeesEarned;
+
+            lastVaultBalance = newVaultBalance;
+            lastUpdated = block.timestamp;
+
+            emit Events.YieldAccrued(newYield, newFeesEarned);
+        }
+    }
+
+    function _deposit(
+        uint256 assets,
+        address receiver,
+        address depositor
+    ) internal returns (uint256 shares) {
+        _accrueYield();
+
+        require((shares = previewDeposit(assets)) != 0, "ZERO_SHARES");
+
+        // Need to transfer before minting or ERC777s could reenter.
+        asset.safeTransferFrom(depositor, address(this), assets);
+
+        // Approve and Deposit the received underlying into Aave v3
+        asset.approve(address(aavePool), assets);
+        aavePool.supply(address(asset), assets, address(this), 0);
+
+        lastVaultBalance = aToken.balanceOf(address(this));
+
+        _mint(receiver, shares);
+
+        emit Deposit(msg.sender, receiver, assets, shares);
+    }
+
     function _mint(
         uint256 shares,
         address receiver,
@@ -213,35 +396,6 @@ contract ATokenVault is ERC4626, Ownable {
         _mint(receiver, shares);
 
         emit Deposit(msg.sender, receiver, assets, shares);
-    }
-
-    function withdraw(
-        uint256 assets,
-        address receiver,
-        address owner
-    ) public override returns (uint256 shares) {
-        shares = _withdraw(assets, receiver, owner, false);
-    }
-
-    function withdrawWithSig(
-        uint256 assets,
-        address receiver,
-        address owner,
-        DataTypes.EIP712Signature calldata sig
-    ) public returns (uint256 shares) {
-        unchecked {
-            MetaTxHelpers._validateRecoveredAddress(
-                MetaTxHelpers._calculateDigest(
-                    keccak256(
-                        abi.encode(WITHDRAW_WITH_SIG_TYPEHASH, assets, receiver, owner, sigNonces[owner]++, sig.deadline)
-                    ),
-                    DOMAIN_SEPARATOR()
-                ),
-                owner,
-                sig
-            );
-        }
-        shares = _withdraw(assets, receiver, owner, true);
     }
 
     function _withdraw(
@@ -271,33 +425,6 @@ contract ATokenVault is ERC4626, Ownable {
         // Withdraw assets from Aave v3 and send to receiver
         aavePool.withdraw(address(asset), assets, receiver);
         lastVaultBalance = aToken.balanceOf(address(this));
-    }
-
-    function redeem(
-        uint256 shares,
-        address receiver,
-        address owner
-    ) public override returns (uint256 assets) {
-        assets = _redeem(shares, receiver, owner, false);
-    }
-
-    function redeemWithSig(
-        uint256 shares,
-        address receiver,
-        address owner,
-        DataTypes.EIP712Signature calldata sig
-    ) public returns (uint256 assets) {
-        unchecked {
-            MetaTxHelpers._validateRecoveredAddress(
-                MetaTxHelpers._calculateDigest(
-                    keccak256(abi.encode(REDEEM_WITH_SIG_TYPEHASH, shares, receiver, owner, sigNonces[owner]++, sig.deadline)),
-                    DOMAIN_SEPARATOR()
-                ),
-                owner,
-                sig
-            );
-        }
-        assets = _redeem(shares, receiver, owner, true);
     }
 
     function _redeem(
@@ -330,51 +457,6 @@ contract ATokenVault is ERC4626, Ownable {
     }
 
     /*//////////////////////////////////////////////////////////////
-                          ONLY OWNER FUNCTIONS
-    //////////////////////////////////////////////////////////////*/
-
-    function setFee(uint256 _newFee) public onlyOwner {
-        if (_newFee > SCALE) revert Errors.FeeTooHigh();
-
-        uint256 oldFee = fee;
-        fee = _newFee;
-
-        emit Events.FeeUpdated(oldFee, _newFee);
-    }
-
-    function updateAavePool() public onlyOwner {
-        address newPool = POOL_ADDRESSES_PROVIDER.getPool();
-        aavePool = IPool(newPool);
-
-        emit Events.AavePoolUpdated(newPool);
-    }
-
-    // Fees are accrued and claimable in aToken form
-    function withdrawFees(address to, uint256 amount) public onlyOwner {
-        uint256 currentFees = getCurrentFees();
-        if (amount > currentFees) revert Errors.InsufficientFees(); // will underflow below anyway, error msg for clarity
-
-        accumulatedFees = currentFees - amount;
-        lastVaultBalance = aToken.balanceOf(address(this)) - amount;
-        lastUpdated = block.timestamp;
-
-        aToken.transfer(to, amount);
-
-        emit Events.FeesWithdrawn(to, amount);
-    }
-
-    function claimAllAaveRewards(address to) public onlyOwner {
-        if (to == address(0)) revert Errors.CannotSendRewardsToZeroAddress();
-
-        address[] memory assets = new address[](1);
-        assets[0] = address(aToken);
-
-        (address[] memory rewardsList, uint256[] memory claimedAmounts) = REWARDS_CONTROLLER.claimAllRewards(assets, to);
-
-        emit Events.AaveRewardsClaimed(to, rewardsList, claimedAmounts);
-    }
-
-    /*//////////////////////////////////////////////////////////////
                           VIEW FUNCTIONS
     //////////////////////////////////////////////////////////////*/
 
@@ -394,26 +476,6 @@ contract ATokenVault is ERC4626, Ownable {
             uint256 newFees = newYield.mulDivDown(fee, SCALE);
 
             return accumulatedFees + newFees;
-        }
-    }
-
-    /*//////////////////////////////////////////////////////////////
-                          INTERNAL FUNCTIONS
-    //////////////////////////////////////////////////////////////*/
-
-    function _accrueYield() internal {
-        // Fees are accrued and claimable in aToken form
-        if (block.timestamp != lastUpdated) {
-            uint256 newVaultBalance = aToken.balanceOf(address(this));
-            uint256 newYield = newVaultBalance - lastVaultBalance;
-            uint256 newFeesEarned = newYield.mulDivDown(fee, SCALE);
-
-            accumulatedFees += newFeesEarned;
-
-            lastVaultBalance = newVaultBalance;
-            lastUpdated = block.timestamp;
-
-            emit Events.YieldAccrued(newYield, newFeesEarned);
         }
     }
 }
