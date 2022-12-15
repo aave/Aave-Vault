b983235641ecaa075d7a11b1d983462cc3555ae8
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2022-12-15 18:00:10 +0200

    Rev 1 and 2: tests done and passing

--- test/ATokenVaultMocks.t.sol
+++ test/ATokenVaultMocks.t.sol
@@ -15,11 +15,34 @@ import {MockAToken} from "./mocks/MockAToken.sol";
 import {MockAavePool} from "./mocks/MockAavePool.sol";
 import {MockDAI} from "./mocks/MockDAI.sol";
 
-contract ATokenVaultPropertiesTest is ATokenVaultBaseTest {
+// import constants
+import "../src/libraries/Constants.sol";
+
+contract ATokenVaultMocksTest is ATokenVaultBaseTest {
     MockAavePoolAddressesProvider poolAddrProvider;
     MockAavePool pool;
     MockAToken aDai;
     MockDAI dai;
+
+    uint256 internal constant IS_ACTIVE_START_BIT_POSITION = 56;
+    uint256 internal constant IS_FROZEN_START_BIT_POSITION = 57;
+    uint256 internal constant IS_PAUSED_START_BIT_POSITION = 60;
+    uint256 internal constant SUPPLY_CAP_UNSCALED = 420;
+
+    uint256 internal constant RESERVE_CONFIG_MAP_UNCAPPED_ACTIVE =
+        (0 & AAVE_ACTIVE_MASK) | (1 << IS_ACTIVE_START_BIT_POSITION);
+
+    uint256 internal constant RESERVE_CONFIG_MAP_CAPPED_ACTIVE = (
+        RESERVE_CONFIG_MAP_UNCAPPED_ACTIVE & AAVE_SUPPLY_CAP_MASK
+    ) | (SUPPLY_CAP_UNSCALED << AAVE_SUPPLY_CAP_BIT_POSITION);
+
+    uint256 internal constant RESERVE_CONFIG_MAP_INACTIVE = (0 & AAVE_ACTIVE_MASK) | (0 << IS_ACTIVE_START_BIT_POSITION);
+
+    uint256 internal constant RESERVE_CONFIG_MAP_FROZEN =
+        (RESERVE_CONFIG_MAP_UNCAPPED_ACTIVE & AAVE_FROZEN_MASK) | (1 << IS_FROZEN_START_BIT_POSITION);
+
+    uint256 internal constant RESERVE_CONFIG_MAP_PAUSED =
+        (RESERVE_CONFIG_MAP_UNCAPPED_ACTIVE & AAVE_PAUSED_MASK) | (1 << IS_PAUSED_START_BIT_POSITION);
 
     // Tested in fork tests - not needed in mock tests
     address fakeIncentivesController = address(101010101);
@@ -46,19 +69,66 @@ contract ATokenVaultPropertiesTest is ATokenVaultBaseTest {
     //////////////////////////////////////////////////////////////*/
 
     function testMaxDepositAaveUncappedSupply() public {
+        pool.setReserveConfigMap(RESERVE_CONFIG_MAP_UNCAPPED_ACTIVE);
         uint256 maxDeposit = vault.maxDeposit(ALICE);
         assertEq(maxDeposit, type(uint256).max);
     }
 
-    function testMaxDepositAaveCappedSupply() public {}
+    function testMaxDepositAaveCappedSupply() public {
+        pool.setReserveConfigMap(RESERVE_CONFIG_MAP_CAPPED_ACTIVE);
+        uint256 maxDeposit = vault.maxDeposit(ALICE);
+        assertEq(maxDeposit, SUPPLY_CAP_UNSCALED * 10 ** dai.decimals());
+    }
 
-    function testMaxDepositAaveNotActive() public {}
+    function testMaxDepositAaveInactive() public {
+        pool.setReserveConfigMap(RESERVE_CONFIG_MAP_INACTIVE);
+        uint256 maxDeposit = vault.maxDeposit(ALICE);
+        assertEq(maxDeposit, 0);
+    }
 
-    function testMaxDepositAaveFrozen() public {}
+    function testMaxDepositAaveFrozen() public {
+        pool.setReserveConfigMap(RESERVE_CONFIG_MAP_FROZEN);
+        uint256 maxDeposit = vault.maxDeposit(ALICE);
+        assertEq(maxDeposit, 0);
+    }
 
-    function testMaxDepositAavePaused() public {}
+    function testMaxDepositAavePaused() public {
+        pool.setReserveConfigMap(RESERVE_CONFIG_MAP_PAUSED);
+        uint256 maxDeposit = vault.maxDeposit(ALICE);
+        assertEq(maxDeposit, 0);
+    }
 
     /*//////////////////////////////////////////////////////////////
                                 MAX MINT
     //////////////////////////////////////////////////////////////*/
+
+    function testMaxMintAaveUncappedSupply() public {
+        pool.setReserveConfigMap(RESERVE_CONFIG_MAP_UNCAPPED_ACTIVE);
+        uint256 maxMint = vault.maxMint(ALICE);
+        assertEq(maxMint, type(uint256).max);
+    }
+
+    function testMaxMintAaveCappedSupply() public {
+        pool.setReserveConfigMap(RESERVE_CONFIG_MAP_CAPPED_ACTIVE);
+        uint256 maxMint = vault.maxMint(ALICE);
+        assertEq(maxMint, SUPPLY_CAP_UNSCALED * 10 ** dai.decimals());
+    }
+
+    function testMaxMintAaveInactive() public {
+        pool.setReserveConfigMap(RESERVE_CONFIG_MAP_INACTIVE);
+        uint256 maxMint = vault.maxMint(ALICE);
+        assertEq(maxMint, 0);
+    }
+
+    function testMaxMintAaveFrozen() public {
+        pool.setReserveConfigMap(RESERVE_CONFIG_MAP_FROZEN);
+        uint256 maxMint = vault.maxMint(ALICE);
+        assertEq(maxMint, 0);
+    }
+
+    function testMaxMintAavePaused() public {
+        pool.setReserveConfigMap(RESERVE_CONFIG_MAP_PAUSED);
+        uint256 maxMint = vault.maxMint(ALICE);
+        assertEq(maxMint, 0);
+    }
 }
