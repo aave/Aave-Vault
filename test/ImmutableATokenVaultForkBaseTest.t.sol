b128d4e98a5287930428e46574cef252dfdaeab4
Alan <adonoso@itba.edu.ar>
2025-08-21 14:45:40 -0300

    feat: Immutable aToken Vault (#104)
    * feat: Immutable aToken Vault added
    
    * test: aToken Vault tests adapted to new Immutable Vault
    
    * misc: Correct comment
    
    * feat: Simplified the approach by overriding _disableInitializers
    
    * test: Removal of unused stuff
    
    * feat: Mark functions as virtual & remove unnecessary super._disableInitializers call
    
    * chore: Audit report added for Immutable aToken Vault

---
+++ test/ImmutableATokenVaultForkBaseTest.t.sol
@@ -0,0 +1,151 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import "forge-std/Test.sol";
+import "./utils/Constants.sol";
+import {ERC20} from "@openzeppelin/token/ERC20/ERC20.sol";
+import {IPool} from "@aave-v3-core/interfaces/IPool.sol";
+import {IAToken} from "@aave-v3-core/interfaces/IAToken.sol";
+import {IPoolDataProvider} from "@aave-v3-core/interfaces/IPoolDataProvider.sol";
+import {DataTypes as AaveDataTypes} from "@aave-v3-core/protocol/libraries/types/DataTypes.sol";
+import {WadRayMath} from "@aave-v3-core/protocol/libraries/math/WadRayMath.sol";
+import {ImmutableATokenVaultBaseTest} from "./ImmutableATokenVaultBaseTest.t.sol";
+
+contract ImmutableATokenVaultForkBaseTest is ImmutableATokenVaultBaseTest {
+    // Forked tests using Polygon for Aave v3
+    uint256 polygonFork;
+    uint256 POLYGON_FORK_BLOCK = 42535610;
+
+    ERC20 dai;
+    IAToken aDai;
+
+    function setUp() public virtual override {
+        polygonFork = vm.createFork(vm.envString("POLYGON_RPC_URL"));
+        vm.selectFork(polygonFork);
+        vm.rollFork(POLYGON_FORK_BLOCK);
+
+        dai = ERC20(POLYGON_DAI);
+        aDai = IAToken(POLYGON_ADAI);
+
+        vaultAssetAddress = address(aDai);
+
+        _deploy(POLYGON_DAI, POLYGON_POOL_ADDRESSES_PROVIDER);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                TEST UTILS
+    //////////////////////////////////////////////////////////////*/
+
+    function _deployAndCheckProps() public {
+        _deploy(POLYGON_DAI, POLYGON_POOL_ADDRESSES_PROVIDER);
+        assertEq(address(vault.asset()), POLYGON_DAI);
+        assertEq(address(vault.ATOKEN()), POLYGON_ADAI);
+        assertEq(address(vault.AAVE_POOL()), POLYGON_AAVE_POOL);
+        assertEq(vault.owner(), OWNER);
+    }
+
+    function _transferFromUser(address user, uint256 amount) public {
+        deal(address(dai), user, amount);
+        vm.startPrank(user);
+        dai.approve(POLYGON_AAVE_POOL, amount);
+        IPool(POLYGON_AAVE_POOL).supply(address(dai), amount, address(vault), 0);
+        vm.stopPrank();
+    }
+
+    function _depositFromUser(address user, uint256 amount) public {
+        deal(address(dai), user, amount);
+
+        vm.startPrank(user);
+        dai.approve(address(vault), amount);
+        vault.deposit(amount, user);
+        vm.stopPrank();
+    }
+
+    function _withdrawFromUser(address user, uint256 amount) public {
+        // If amount is 0, withdraw max for user
+        if (amount == 0) amount = vault.maxWithdraw(user);
+        vm.startPrank(user);
+        vault.withdraw(amount, user, user);
+        vm.stopPrank();
+    }
+
+    function _redeemFromUser(address user, uint256 shares) public {
+        if (shares == 0) shares = vault.maxRedeem(user);
+        vm.startPrank(user);
+        vault.redeem(shares, user, user);
+        vm.stopPrank();
+    }
+
+    function _withdrawFees(uint256 amount) public {
+        if (amount == 0) amount = vault.getClaimableFees();
+        vm.startPrank(OWNER);
+        vault.withdrawFees(OWNER, amount);
+        vm.stopPrank();
+    }
+
+    function _accrueYieldInVault(uint256 yieldAmountToAccrue) public {
+        require(yieldAmountToAccrue > 0, "TEST: FEES ACCRUED MUST BE > 0");
+
+        deal(address(dai), OWNER, yieldAmountToAccrue);
+
+        vm.startPrank(OWNER);
+        dai.approve(POLYGON_AAVE_POOL, yieldAmountToAccrue);
+        IPool(POLYGON_AAVE_POOL).supply(address(dai), yieldAmountToAccrue, OWNER, 0);
+
+        // NOTE: reducing by 1 because final vault balance is over by 1 for some reason
+        yieldAmountToAccrue -= 1;
+        aDai.transfer(address(vault), yieldAmountToAccrue);
+        vm.stopPrank();
+
+        assertGt(aDai.balanceOf(address(vault)), yieldAmountToAccrue);
+    }
+
+    function _accrueFeesInVault(uint256 feeAmountToAccrue) public {
+        require(feeAmountToAccrue > 0, "TEST: FEES ACCRUED MUST BE > 0");
+        uint256 daiAmount = (feeAmountToAccrue * SCALE) / vault.getFee();
+
+        deal(address(dai), ALICE, daiAmount + ONE);
+
+        vm.startPrank(ALICE);
+        dai.approve(address(vault), ONE);
+        vault.deposit(ONE, ALICE);
+        dai.approve(POLYGON_AAVE_POOL, daiAmount);
+        IPool(POLYGON_AAVE_POOL).supply(address(dai), daiAmount, ALICE, 0);
+        aDai.transfer(address(vault), daiAmount);
+        skip(1);
+
+        vault.withdraw(vault.maxWithdraw(ALICE), ALICE, ALICE);
+        vm.stopPrank();
+
+        // Fees will be more than specified in param because of interest earned over time in Aave
+        assertApproxEqRel(vault.getClaimableFees(), feeAmountToAccrue, ONE_BPS);
+    }
+
+    function _getFeesOnAmount(uint256 amount) public view returns (uint256) {
+        return (amount * vault.getFee()) / SCALE;
+    }
+
+    function _maxDaiSuppliableToAave() internal view returns (uint256) {
+        AaveDataTypes.ReserveData memory reserveData = IPool(POLYGON_AAVE_POOL).getReserveData(POLYGON_DAI);
+
+        uint256 reserveConfigMap = reserveData.configuration.data;
+        (, uint256 supplyCap) = IPoolDataProvider(POLYGON_DATA_PROVIDER).getReserveCaps(POLYGON_DAI);
+
+        if (
+            (reserveConfigMap & ~AAVE_ACTIVE_MASK == 0) ||
+            (reserveConfigMap & ~AAVE_FROZEN_MASK != 0) ||
+            (reserveConfigMap & ~AAVE_PAUSED_MASK != 0)
+        ) {
+            return 0;
+        } else if (supplyCap == 0) {
+            return type(uint256).max;
+        } else {
+            return
+                (supplyCap * 10 ** dai.decimals()) -
+                WadRayMath.rayMul(
+                    (aDai.scaledTotalSupply() + uint256(reserveData.accruedToTreasury)),
+                    reserveData.liquidityIndex
+                );
+        }
+    }
+}
---
+++ test/ImmutableATokenVaultMocks.t.sol
@@ -0,0 +1,323 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.10;
+
+import "./utils/Constants.sol";
+import {MockAavePoolAddressesProvider} from "./mocks/MockAavePoolAddressesProvider.sol";
+import {MockAToken} from "./mocks/MockAToken.sol";
+import {MockAavePool} from "./mocks/MockAavePool.sol";
+import {MockDAI} from "./mocks/MockDAI.sol";
+import {ImmutableATokenVaultBaseTest} from "./ImmutableATokenVaultBaseTest.t.sol";
+
+contract ImmutableATokenVaultMocksTest is ImmutableATokenVaultBaseTest {
+    MockAavePoolAddressesProvider poolAddrProvider;
+    MockAavePool pool;
+    MockAToken aDai;
+    MockDAI dai;
+
+    function setUp() public override {
+        dai = new MockDAI();
+        aDai = new MockAToken(address(dai));
+        pool = new MockAavePool(aDai);
+        poolAddrProvider = new MockAavePoolAddressesProvider(address(pool));
+
+        pool.setReserveConfigMap(RESERVE_CONFIG_MAP_UNCAPPED_ACTIVE);
+        _deploy(address(dai), address(poolAddrProvider));
+        // Remove initial supply
+        vm.startPrank(address(vault));
+        vault.redeem(vault.balanceOf(address(vault)), address(vault), address(vault));
+        vm.stopPrank();
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                MAX DEPOSIT
+    //////////////////////////////////////////////////////////////*/
+
+    function testMaxDepositAaveUncappedSupply() public {
+        pool.setReserveConfigMap(RESERVE_CONFIG_MAP_UNCAPPED_ACTIVE);
+        uint256 maxDeposit = vault.maxDeposit(ALICE);
+        assertEq(maxDeposit, type(uint256).max);
+    }
+
+    function testMaxDepositAaveCappedSupply() public {
+        pool.setReserveConfigMap(RESERVE_CONFIG_MAP_CAPPED_ACTIVE);
+        uint256 maxDeposit = vault.maxDeposit(ALICE);
+        assertEq(maxDeposit, SUPPLY_CAP_UNSCALED * 10 ** dai.decimals());
+    }
+
+    function testMaxDepositAaveInactive() public {
+        pool.setReserveConfigMap(RESERVE_CONFIG_MAP_INACTIVE);
+        uint256 maxDeposit = vault.maxDeposit(ALICE);
+        assertEq(maxDeposit, 0);
+    }
+
+    function testMaxDepositAaveFrozen() public {
+        pool.setReserveConfigMap(RESERVE_CONFIG_MAP_FROZEN);
+        uint256 maxDeposit = vault.maxDeposit(ALICE);
+        assertEq(maxDeposit, 0);
+    }
+
+    function testMaxDepositAavePaused() public {
+        pool.setReserveConfigMap(RESERVE_CONFIG_MAP_PAUSED);
+        uint256 maxDeposit = vault.maxDeposit(ALICE);
+        assertEq(maxDeposit, 0);
+    }
+
+    function testMaxDepositAaveCappedWithSomeSupply() public {
+        pool.setReserveConfigMap(RESERVE_CONFIG_MAP_CAPPED_ACTIVE);
+        uint256 supplyCapWithDecimals = SUPPLY_CAP_UNSCALED * 10 ** dai.decimals();
+        assertEq(vault.maxDeposit(ALICE), supplyCapWithDecimals);
+
+        // ALICE deposit TEN
+        deal(address(dai), ALICE, TEN);
+        vm.startPrank(ALICE);
+        dai.approve(address(vault), TEN);
+        vault.deposit(TEN, ALICE);
+        vm.stopPrank();
+
+        assertEq(vault.maxDeposit(ALICE), supplyCapWithDecimals - TEN);
+    }
+
+    function testMaxDepositAaveCappedBelowCurrentSupply() public {
+        assertEq(vault.maxDeposit(ALICE), type(uint256).max);
+
+        // ALICE deposit TEN
+        deal(address(dai), ALICE, TEN);
+        vm.startPrank(ALICE);
+        dai.approve(address(vault), TEN);
+        vault.deposit(TEN, ALICE);
+        vm.stopPrank();
+
+        // SupplyCap of ONE
+        pool.setReserveConfigMap(RESERVE_CONFIG_MAP_SHORT_CAPPED_ACTIVE);
+        assertEq(vault.maxDeposit(ALICE), 0);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                MAX MINT
+    //////////////////////////////////////////////////////////////*/
+
+    function testMaxMintAaveUncappedSupply() public {
+        pool.setReserveConfigMap(RESERVE_CONFIG_MAP_UNCAPPED_ACTIVE);
+        uint256 maxMint = vault.maxMint(ALICE);
+        assertEq(maxMint, type(uint256).max);
+    }
+
+    function testMaxMintAaveCappedSupply() public {
+        pool.setReserveConfigMap(RESERVE_CONFIG_MAP_CAPPED_ACTIVE);
+        uint256 maxMint = vault.maxMint(ALICE);
+        assertEq(maxMint, SUPPLY_CAP_UNSCALED * 10 ** dai.decimals());
+    }
+
+    function testMaxMintAaveInactive() public {
+        pool.setReserveConfigMap(RESERVE_CONFIG_MAP_INACTIVE);
+        uint256 maxMint = vault.maxMint(ALICE);
+        assertEq(maxMint, 0);
+    }
+
+    function testMaxMintAaveFrozen() public {
+        pool.setReserveConfigMap(RESERVE_CONFIG_MAP_FROZEN);
+        uint256 maxMint = vault.maxMint(ALICE);
+        assertEq(maxMint, 0);
+    }
+
+    function testMaxMintAavePaused() public {
+        pool.setReserveConfigMap(RESERVE_CONFIG_MAP_PAUSED);
+        uint256 maxMint = vault.maxMint(ALICE);
+        assertEq(maxMint, 0);
+    }
+
+    function testMaxMintAaveCappedWithSomeSupply() public {
+        pool.setReserveConfigMap(RESERVE_CONFIG_MAP_CAPPED_ACTIVE);
+        uint256 supplyCapWithDecimals = SUPPLY_CAP_UNSCALED * 10 ** dai.decimals();
+        assertEq(vault.maxMint(ALICE), supplyCapWithDecimals);
+
+        // ALICE deposit TEN
+        deal(address(dai), ALICE, TEN);
+        vm.startPrank(ALICE);
+        dai.approve(address(vault), TEN);
+        vault.deposit(TEN, ALICE);
+        vm.stopPrank();
+
+        assertEq(vault.maxMint(ALICE), supplyCapWithDecimals - TEN);
+    }
+
+    function testMaxMintAaveCappedBelowCurrentSupply() public {
+        assertEq(vault.maxMint(ALICE), type(uint256).max);
+
+        // ALICE deposit TEN
+        deal(address(dai), ALICE, TEN);
+        vm.startPrank(ALICE);
+        dai.approve(address(vault), TEN);
+        vault.deposit(TEN, ALICE);
+        vm.stopPrank();
+
+        // SupplyCap of ONE
+        pool.setReserveConfigMap(RESERVE_CONFIG_MAP_SHORT_CAPPED_ACTIVE);
+        assertEq(vault.maxMint(ALICE), 0);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                MAX WITHDRAW
+    //////////////////////////////////////////////////////////////*/
+
+    function testMaxWithdrawAaveMoreThanEnoughLiquidity() public {
+        pool.setReserveConfigMap(RESERVE_CONFIG_MAP_UNCAPPED_ACTIVE);
+
+        // ALICE deposit ONE
+        deal(address(dai), ALICE, ONE);
+        vm.startPrank(ALICE);
+        dai.approve(address(vault), ONE);
+        vault.deposit(ONE, ALICE);
+        vm.stopPrank();
+
+        // Pool utilization is 2*ONE
+        vm.mockCall(address(dai), abi.encodeWithSelector(dai.balanceOf.selector, address(aDai)), abi.encode(2 * ONE));
+
+        uint256 maxWithdraw = vault.maxWithdraw(ALICE);
+        assertEq(maxWithdraw, ONE);
+    }
+
+    function testMaxWithdrawAaveEnoughLiquidity() public {
+        pool.setReserveConfigMap(RESERVE_CONFIG_MAP_UNCAPPED_ACTIVE);
+
+        // ALICE deposit ONE
+        deal(address(dai), ALICE, ONE);
+        vm.startPrank(ALICE);
+        dai.approve(address(vault), ONE);
+        vault.deposit(ONE, ALICE);
+        vm.stopPrank();
+
+        // Pool utilization is ONE
+        vm.mockCall(address(dai), abi.encodeWithSelector(dai.balanceOf.selector, address(aDai)), abi.encode(ONE));
+
+        uint256 maxWithdraw = vault.maxWithdraw(ALICE);
+        assertEq(maxWithdraw, ONE);
+    }
+
+    function testMaxWithdrawAaveNotEnoughLiquidity() public {
+        pool.setReserveConfigMap(RESERVE_CONFIG_MAP_UNCAPPED_ACTIVE);
+
+        // ALICE deposit ONE
+        deal(address(dai), ALICE, ONE);
+        vm.startPrank(ALICE);
+        dai.approve(address(vault), ONE);
+        vault.deposit(ONE, ALICE);
+        vm.stopPrank();
+
+        // Pool utilization is ONE - 1
+        vm.mockCall(address(dai), abi.encodeWithSelector(dai.balanceOf.selector, address(aDai)), abi.encode(ONE - 1));
+
+        uint256 maxWithdraw = vault.maxWithdraw(ALICE);
+        assertEq(maxWithdraw, ONE - 1);
+    }
+
+    function testMaxWithdrawAaveNoLiquidity() public {
+        pool.setReserveConfigMap(RESERVE_CONFIG_MAP_UNCAPPED_ACTIVE);
+
+        // ALICE deposit ONE
+        deal(address(dai), ALICE, ONE);
+        vm.startPrank(ALICE);
+        dai.approve(address(vault), ONE);
+        vault.deposit(ONE, ALICE);
+        vm.stopPrank();
+
+        // Pool utilization is 0
+        vm.mockCall(address(dai), abi.encodeWithSelector(dai.balanceOf.selector, address(aDai)), abi.encode(0));
+
+        uint256 maxWithdraw = vault.maxWithdraw(ALICE);
+        assertEq(maxWithdraw, 0);
+    }
+
+    function testMaxWithdrawAaveInactive() public {
+        pool.setReserveConfigMap(RESERVE_CONFIG_MAP_INACTIVE);
+        uint256 maxWithdraw = vault.maxWithdraw(ALICE);
+        assertEq(maxWithdraw, 0);
+    }
+
+    function testMaxWithdrawAavePaused() public {
+        pool.setReserveConfigMap(RESERVE_CONFIG_MAP_PAUSED);
+        uint256 maxWithdraw = vault.maxWithdraw(ALICE);
+        assertEq(maxWithdraw, 0);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                MAX REDEEM
+    //////////////////////////////////////////////////////////////*/
+
+    function testMaxRedeemAaveMoreThanEnoughLiquidity() public {
+        pool.setReserveConfigMap(RESERVE_CONFIG_MAP_UNCAPPED_ACTIVE);
+
+        // ALICE deposit ONE
+        deal(address(dai), ALICE, ONE);
+        vm.startPrank(ALICE);
+        dai.approve(address(vault), ONE);
+        vault.deposit(ONE, ALICE);
+        vm.stopPrank();
+
+        // Pool utilization is 2*ONE
+        vm.mockCall(address(dai), abi.encodeWithSelector(dai.balanceOf.selector, address(aDai)), abi.encode(2 * ONE));
+        uint256 maxRedeem = vault.maxRedeem(ALICE);
+        assertEq(maxRedeem, vault.convertToShares(ONE));
+    }
+
+    function testMaxRedeemAaveEnoughLiquidity() public {
+        pool.setReserveConfigMap(RESERVE_CONFIG_MAP_UNCAPPED_ACTIVE);
+
+        // ALICE deposit ONE
+        deal(address(dai), ALICE, ONE);
+        vm.startPrank(ALICE);
+        dai.approve(address(vault), ONE);
+        vault.deposit(ONE, ALICE);
+        vm.stopPrank();
+
+        // Pool utilization is ONE
+        vm.mockCall(address(dai), abi.encodeWithSelector(dai.balanceOf.selector, address(aDai)), abi.encode(ONE));
+        uint256 maxRedeem = vault.maxRedeem(ALICE);
+        assertEq(maxRedeem, vault.convertToShares(ONE));
+    }
+
+    function testMaxRedeemAaveNotEnoughLiquidity() public {
+        pool.setReserveConfigMap(RESERVE_CONFIG_MAP_UNCAPPED_ACTIVE);
+
+        // ALICE deposit ONE
+        deal(address(dai), ALICE, ONE);
+        vm.startPrank(ALICE);
+        dai.approve(address(vault), ONE);
+        vault.deposit(ONE, ALICE);
+        vm.stopPrank();
+
+        // Pool utilization is ONE - 1
+        vm.mockCall(address(dai), abi.encodeWithSelector(dai.balanceOf.selector, address(aDai)), abi.encode(ONE - 1));
+        uint256 maxRedeem = vault.maxRedeem(ALICE);
+        assertEq(maxRedeem, vault.convertToShares(ONE - 1));
+    }
+
+    function testMaxRedeemAaveNoLiquidity() public {
+        pool.setReserveConfigMap(RESERVE_CONFIG_MAP_UNCAPPED_ACTIVE);
+
+        // ALICE deposit ONE
+        deal(address(dai), ALICE, ONE);
+        vm.startPrank(ALICE);
+        dai.approve(address(vault), ONE);
+        vault.deposit(ONE, ALICE);
+        vm.stopPrank();
+
+        // Pool utilization is 0
+        vm.mockCall(address(dai), abi.encodeWithSelector(dai.balanceOf.selector, address(aDai)), abi.encode(0));
+        uint256 maxRedeem = vault.maxRedeem(ALICE);
+        assertEq(maxRedeem, 0);
+    }
+
+    function testMaxRedeemAaveInactive() public {
+        pool.setReserveConfigMap(RESERVE_CONFIG_MAP_INACTIVE);
+        uint256 maxRedeem = vault.maxRedeem(ALICE);
+        assertEq(maxRedeem, 0);
+    }
+
+    function testMaxRedeemAavePaused() public {
+        pool.setReserveConfigMap(RESERVE_CONFIG_MAP_PAUSED);
+        uint256 maxRedeem = vault.maxRedeem(ALICE);
+        assertEq(maxRedeem, 0);
+    }
+}
